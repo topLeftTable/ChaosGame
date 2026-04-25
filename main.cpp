aeaac63c5442c981d5a44f403d17167adaf953ec
ezicle shabang <ezikiels007@gmail.com>
2026-04-25 00:23:05 -0700

    at long last^2, the text renderer that sorta works maybe

--- main.cpp
+++ main.cpp
@@ -1,11 +1,5 @@
-  /*
-  **************************************
-  Project By LoganMD, jalenzwatkins,
-  WaitingKeptYouHuh, and deathsdebtor
-  **************************************
-  */
-
 // Include important C++ libraries here
+
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <iostream>
@@ -16,25 +10,19 @@
 // Make the code easier to type with "using namespace"
 using namespace sf;
 using namespace std;
-
+//bool update;
 int main()
 {
-
-  // Seed the random number generator
-  srand(time(nullptr));
-
+  
+  srand(time(0));
   // Create a video mode object
   VideoMode vm(1920, 1080);
 
   // Create and open a window for the game
   RenderWindow window(vm, "Chaos Game!!", Style::Default);
 
-  // Coordinate points forming initial shape
   vector<Vector2f> vertices;
-  // Random points generated during calculations
   vector<Vector2f> points;
-
-  // Start the game loop
   while (window.isOpen())
   {
 
@@ -54,41 +42,41 @@ int main()
         window.close();
       }
 
-      // Quit the game when ESC key is pressed
-      if (Keyboard::isKeyPressed(Keyboard::Escape))
-      {
-        window.close();
-      }
-
       if (event.type == sf::Event::MouseButtonPressed)
       {
-
         if (event.mouseButton.button == sf::Mouse::Left)
         {
           std::cout << "the left button was pressed" << std::endl;
           std::cout << "mouse x: " << event.mouseButton.x << std::endl;
           std::cout << "mouse y: " << event.mouseButton.y << std::endl;
-
-          // Possible vertices max out at 10
           if (vertices.size() < 10)
           {
-            // store vertices of Left Mouse clicks
             vertices.push_back(
                 Vector2f(event.mouseButton.x, event.mouseButton.y));
           }
         }
-
         else if (event.mouseButton.button == sf::Mouse::Right && points.size() == 0)
-        {
+          {
+          /// fourth click
+
           /// push back to points vector
+
           for (int i = 0; i < vertices.size(); i++)
           {
             points.push_back(vertices[i]);
           }
-        }
-
+          }
       }
+    }
 
+
+//    DEBUG: POINTERPOSY
+    cout << " Event Mousebutton: " << event.mouseButton.y << endl; 
+//    cout << " Vector Mousebutton: " << vector2i.y << endl;
+
+    if (Keyboard::isKeyPressed(Keyboard::Escape))
+    {
+      window.close();
     }
 
     /*
@@ -100,23 +88,73 @@ int main()
     if (points.size() > 0)
     {
       /// generate more point(s)
+      /// select random vertex
+
+      /// calculate midpoint between random vertex and the last point in the
+      /// vector
+
+      /// push back the newly generated coord.
+
+      // NOT GONNA LIE WE ALL STRUGGLED AND WORKED ON THIS
+
+        int vert = vertices.size();
+        double r;
+
+        switch (vert)
+        {
+
+            case 3:
+             r = .5;
+              break;
+
+            case 4:
+              r = .5;
+              break;
+
+            case 5:
+              r = .618;
+              break;
+
+             case 6:
+              r = .667;
+               break;
+
+             case 7:
+               r = .692;
+                break;
+
+             case 8:
+               r = .707;
+               break;
+
+             case 9:
+               r = .742;
+               break;
+
+             case 10:
+               r = .764;
+               break;
+
+               default:
+               break;              
+
+        }
+
+
+
+
       int randomInt;
       Vector2f randomVec;
+      Vector2f randomVec2;
       Vector2f tempVec;
-
-      /// select random vertex
-      randomInt = rand() % vertices.size();
+      
+      randomInt = rand() % (vertices.size());
       randomVec = vertices[randomInt];
+      
 
-      // ratio formula for any polygon of n-corner points
-      double r = vertices.size() / (vertices.size() + 3.0);
-
-      /// calculate midpoint between random vertex
-      /// and the last point in the vector
       tempVec.x = (randomVec.x + points[points.size() - 1].x) * r;
       tempVec.y = (randomVec.y + points[points.size() - 1].y) * r;
 
-      /// push back the newly generated coord.
       points.push_back(tempVec);
     }
 
@@ -131,7 +169,57 @@ int main()
 //    WaitingKeptYouHuh: Text Rendering or something
 //    Мрак привет
 
-    string str = "Pointer Position: " + to_string(event.mouseButton.x) + "," + to_string(event.mouseButton.y) + "\nAmount of Points: " + to_string(vertices.size());
+    string selShape;
+    selShape = "shape";
+    switch (vertices.size())
+    {
+       case 3:
+          selShape = "Triangle";
+          break;
+       case 4:
+          selShape = "Square";
+          break;
+       case 5:
+          selShape = "Pentagon";
+          break;
+       case 6:
+          selShape = "Hexagon";
+          break;
+       case 7:
+          selShape = "Heptagon";
+          break;
+       case 8:
+          selShape = "Octagon";
+          break;
+       case 9:
+          selShape = "Nonagon";
+          break;
+       case 10:
+          selShape = "Decagon";
+          break;
+    }
+
+    double ptrPosX = 0.00;
+    double ptrPosY = 0.00;
+
+    bool update;    
+    cout << update << endl;
+    if(event.type == Event::MouseMoved){update = 1;} else if (event.type == Event::MouseLeft){update = 0;}
+    
+    if (update) 
+    {
+       if (event.type == Event::MouseButtonPressed or event.type == Event::MouseButtonReleased)
+       {
+          ptrPosX = event.mouseButton.x; ptrPosY = event.mouseButton.y;
+       }
+       else
+       {
+          ptrPosX = event.mouseMove.x; ptrPosY = event.mouseMove.y;
+       }
+    }
+    cout << update << endl << endl;   
+
+    string str = "Pointer Position : " + to_string(ptrPosX) + "," + to_string(ptrPosY) + "\nAmount of Points: " + to_string(vertices.size()) + "\nRight Click To Draw a " + selShape;
     Text msg;
     Font font;
     font.loadFromFile("fonts/IBMPlexMono-Regular.ttf");
@@ -148,34 +236,30 @@ int main()
        textBox.height / 2.0f);
     msg.setPosition(220,50);
 
-//    debug 4 textrender
-//    FIXME .Y COORDINATE IS ACTING WEIRD FOR NO DISCERNABLE REASON
-cout << event.mouseButton.y << endl;
-
     for (int i = 0; i < vertices.size(); i++)
     {
-      RectangleShape rect(Vector2f(10, 10));
+      RectangleShape rect(Vector2f(5, 5));
       rect.setPosition(Vector2f(vertices[i].x, vertices[i].y));
+
       rect.setFillColor(Color::Blue);
       window.draw(rect);
     }
 
-    /// Draw points
+    //points.push_back(Vector2f(15, 15));
+
+    /// TODO:  Draw points
     if (points.size() > 0)
     {
-      for (int i = 0; i < points.size(); i++)
+      for (Vector2f point : points)
       {
-        RectangleShape rect(Vector2f(10, 10));
-        rect.setPosition(points[i]);
+        RectangleShape rect(Vector2f(1, 1));
+        rect.setPosition(point);
         rect.setFillColor(Color::Green);
         window.draw(rect);
       }
     }
 
-
     window.draw(msg);
-//    window.display(textBox);
-
 
     window.display();
   }

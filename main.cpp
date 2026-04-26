d1c3f9be234be787e9c8bdd014c1ed7943c2255e
LoganMD <logan@logandev.org>
2026-04-25 17:45:35 -0700

    the logan patch(tm)

--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
-  /*
-  **************************************
-  Project By Logan Doose, Jalen Watkins,
-  Ezekiel Shahbaz, and Deaven Niccum
-  **************************************
-  */
+/*
+**************************************
+Project By Logan Doose, Jalen Watkins,
+Ezekiel Shahbaz, and Deaven Niccum
+**************************************
+*/
 
 // Include important C++ libraries here
-#include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
 #include <iostream>
 #include <sstream>
-#include <vector>
 #include <string>
+#include <vector>
 
 // Make the code easier to type with "using namespace"
 using namespace sf;
@@ -19,7 +19,7 @@ using namespace std;
 
 int main()
 {
-//  Declarations and initializaation for text renderer
+  //  Declarations and initialization for text renderer
   string str;
   Text msg;
   Font font;
@@ -27,7 +27,7 @@ int main()
   msg.setFont(font);
   msg.setCharacterSize(25);
   msg.setFillColor(Color::Magenta);
-  msg.setPosition(50,50);
+  msg.setPosition(50, 50);
 
   FloatRect msgBounds;
   RectangleShape msgBackground;
@@ -40,23 +40,8 @@ int main()
   // Seed the random number generator
   srand(time(nullptr));
 
-  string str;
-  Text msg;
-  Font font;
-  font.loadFromFile("fonts/IBMPlexMono-Regular.ttf");
-  msg.setFont(font);
-  msg.setCharacterSize(25);
-  msg.setFillColor(Color::Magenta);
-  msg.setPosition(50, 50);
-
-  int ptrPosX;
-  int ptrPosY;
-
   string selShape;
 
-  bool update;
-
-  srand(time(0));
   // Create a video mode object
   VideoMode vm(1920, 1080);
 
@@ -94,10 +79,10 @@ int main()
         window.close();
       }
 
-      if (event.type == sf::Event::MouseButtonPressed)
+      if (event.type == Event::MouseButtonPressed)
       {
 
-        if (event.mouseButton.button == sf::Mouse::Left)
+        if (event.mouseButton.button == Mouse::Left)
         {
           std::cout << "the left button was pressed" << std::endl;
           std::cout << "mouse x: " << event.mouseButton.x << std::endl;
@@ -111,8 +96,8 @@ int main()
                 Vector2f(event.mouseButton.x, event.mouseButton.y));
           }
         }
-        else if (event.mouseButton.button == sf::Mouse::Right && points.size() == 0)
-          {
+        else if (event.mouseButton.button == Mouse::Right && points.size() == 0)
+        {
           /// fourth click
 
           /// push back to points vector
@@ -121,14 +106,13 @@ int main()
           {
             points.push_back(vertices[i]);
           }
-          }
+        }
       }
     }
 
-
-//    DEBUG: POINTERPOSY
-    cout << " Event Mousebutton: " << event.mouseButton.y << endl; 
-//    cout << " Vector Mousebutton: " << vector2i.y << endl;
+    //    DEBUG: POINTERPOSY
+    // cout << " Event Mousebutton: " << event.mouseButton.y << endl;
+    //    cout << " Vector Mousebutton: " << vector2i.y << endl;
 
     if (Keyboard::isKeyPressed(Keyboard::Escape))
     {
@@ -152,52 +136,6 @@ int main()
       /// push back the newly generated coord.
 
       // NOT GONNA LIE WE ALL STRUGGLED AND WORKED ON THIS
-
-        int vert = vertices.size();
-        double r;
-
-        switch (vert)
-        {
-
-            case 3:
-             r = .5;
-              break;
-
-            case 4:
-              r = .5;
-              break;
-
-            case 5:
-              r = .618;
-              break;
-
-             case 6:
-              r = .667;
-               break;
-
-             case 7:
-               r = .692;
-                break;
-
-             case 8:
-               r = .707;
-               break;
-
-             case 9:
-               r = .742;
-               break;
-
-             case 10:
-               r = .764;
-               break;
-
-               default:
-               break;              
-
-        }
-
-
-
 
       int randomInt;
       Vector2f randomVec;
@@ -228,72 +166,85 @@ int main()
 
     window.clear();
 
-//    Ezekiel: Text Rendering or something
-//    Мрак привет
+    //    Ezekiel: Text Rendering or something
+    //    Мрак привет
 
-//    UI Shape Text Selection
-//    Logic to Select Shape Name
+    //    UI Shape Text Selection
+    //    Logic to Select Shape Name
     selShape = "Shape";
     switch (vertices.size())
     {
-       case 3:
-          selShape = "Triangle";
-          break;
-       case 4:
-          selShape = "Square";
-          break;
-       case 5:
-          selShape = "Pentagon";
-          break;
-       case 6:
-          selShape = "Hexagon";
-          break;
-       case 7:
-          selShape = "Heptagon";
-          break;
-       case 8:
-          selShape = "Octagon";
-          break;
-       case 9:
-          selShape = "Nonagon";
-          break;
-       case 10:
-          selShape = "Decagon";
-          break;
+    case 3:
+      selShape = "Triangle";
+      break;
+    case 4:
+      selShape = "Square";
+      break;
+    case 5:
+      selShape = "Pentagon";
+      break;
+    case 6:
+      selShape = "Hexagon";
+      break;
+    case 7:
+      selShape = "Heptagon";
+      break;
+    case 8:
+      selShape = "Octagon";
+      break;
+    case 9:
+      selShape = "Nonagon";
+      break;
+    case 10:
+      selShape = "Decagon";
+      break;
     }
 
-//   Logic to Determine Whether or Not to Update ptrPos Text
+    //   Logic to Determine Whether or Not to Update ptrPos Text
     if (event.type == Event::MouseMoved)
     {
-       update = 1;
+      update = true;
     }
     else if (event.type == Event::MouseLeft)
     {
-       update = 0;
+      update = false;
     }
 
-    bool update;    
-    cout << update << endl;
-    if(event.type == Event::MouseMoved){update = 1;} else if (event.type == Event::MouseLeft){update = 0;}
-    
-    if (update) 
+    if (event.type == Event::MouseMoved)
     {
-       if (event.type == Event::MouseButtonPressed or event.type == Event::MouseButtonReleased)
-       {
-          ptrPosX = event.mouseButton.x; ptrPosY = event.mouseButton.y;
-       }
-       else
-       {
-          ptrPosX = event.mouseMove.x; ptrPosY = event.mouseMove.y;
-       }
+      update = true;
     }
-    cout << update << endl << endl;   
+    else if (event.type == Event::MouseLeft)
+    {
+      update = false;
+    }
 
-//    Printing
-    str = "Pointer Position : " + to_string(ptrPosX) + "," + to_string(ptrPosY) + "\nAmount of Points: " + to_string(vertices.size()) + "\nRight Click to Draw a " + selShape;
+    if (update)
+    {
+      if (event.type == Event::MouseButtonPressed ||
+          event.type == Event::MouseButtonReleased)
+      {
+        ptrPosX = event.mouseButton.x;
+        ptrPosY = event.mouseButton.y;
+      }
+      else
+      {
+        ptrPosX = event.mouseMove.x;
+        ptrPosY = event.mouseMove.y;
+      }
+    }
+    // cout << update << endl << endl;
+
+    //    Printing
+    str = "Pointer Position : " + to_string(ptrPosX) + "," +
+          to_string(ptrPosY) +
+          "\nAmount of Points: " + to_string(vertices.size()) +
+          "\nRight Click to Draw a " + selShape;
     msg.setString(str);
     msgBounds = msg.getGlobalBounds();
-    msgBackground.setSize(Vector2f(msgBounds.width + 16, msgBounds.height + 16)); // 8px padding (Logan's idea)
+    msgBackground.setSize(
+        Vector2f(msgBounds.width + 16,
+                 msgBounds.height + 16)); // 8px padding (Logan's idea)
     msgBackground.setPosition(Vector2f(msgBounds.left - 8, msgBounds.top - 8));
 
     for (int i = 0; i < vertices.size(); i++)
@@ -305,7 +256,7 @@ int main()
       window.draw(rect);
     }
 
-    //points.push_back(Vector2f(15, 15));
+    // points.push_back(Vector2f(15, 15));
 
     /// TODO:  Draw points
     if (points.size() > 0)
